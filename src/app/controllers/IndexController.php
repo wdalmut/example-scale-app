e4c02061c0c47afaa36e73abc2ffe5b137ea867d
Walter Dal Mut <walter.dalmut@gmail.com>
2014-05-02 16:36:28 +0200

    Update homepage

--- src/app/controllers/IndexController.php
+++ src/app/controllers/IndexController.php
@@ -3,7 +3,7 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
-        $this->view->helloText = "Abbiamo finito il corso!";
+        $this->view->helloText = "E faccio la mia sessione";
     }
 }
 
